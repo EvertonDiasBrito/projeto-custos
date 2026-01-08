a5b35fa69922df08b19998c51bd8bf4e5ab85c63
Everton <evertondiasbrito@gmail.com>
2026-01-08 15:14:21 +0000

    Criando Formulário

--- src/components/pages/NovoProjeto.tsx
+++ src/components/pages/NovoProjeto.tsx
@@ -1,9 +1,4 @@
-function NovoProjeto() {
-    return (
-        <h1>Novo Projeto</h1>
-    )
-}
-export default NovoProjeto
+import ProjetoForm from '../projetos/ProjetoForm'
 import styles from './NovoProjeto.module.css'
 
 function NovoProjeto() {
@@ -11,7 +6,7 @@ function NovoProjeto() {
         <div className={styles.novoprojeto_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <p>Formulário</p>
+            <ProjetoForm/>
         </div>
     )
 }
---
+++ src/components/projetos/ProjetoForm.tsx
@@ -0,0 +1,16 @@
+function ProjetoForm() {
+    return (
+        <form>
+            <div><input type="text" placeholder= "Insira o orçamento total"/></div>
+            <div><input type="number" placeholder="Insira o orçamento total" /></div>
+            <div>
+                <select name="category_id">
+                    <option disabled>Selecione a categoria</option>
+                </select>
+            </div>
+            <div><input type="submit"value="Criar projeto"/></div>
+        </form>
+    );
+}
+
+export default ProjetoForm
