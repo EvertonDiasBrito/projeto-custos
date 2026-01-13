09ef5188167a1c5a1959fe4b5ad10ffdfef9944e
Everton <evertondiasbrito@gmail.com>
2026-01-13 16:20:45 +0000

    Layout da Pg Projetos

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar seus projetos!</p>
-            <LinkButton to="novoprojeto" text="Criar Projeto"/>
+            <LinkButton to="/novoprojeto" text="Criar Projeto"/>
             <img src={savings} alt="Costs" />
         </section>
     )
--- src/components/pages/Projetos.tsx
+++ src/components/pages/Projetos.tsx
@@ -1,14 +1,24 @@
 import { useLocation } from 'react-router-dom'
+
 import Message from './Message'
+import styles from './Projetos.module.css'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
 
 function Projetos() {
   const location = useLocation()
-  let message = location.state?.message
+  const message = location.state?.message
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.projetos_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/novoprojeto" text="Criar Projeto"/>
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 }
